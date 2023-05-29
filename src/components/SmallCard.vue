c6b2690432e4fc2b489247ceb6429d58398880eb
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-29 18:10:20 +0200

    add style to small cards

--- src/components/SmallCard.vue
+++ src/components/SmallCard.vue
@@ -9,13 +9,27 @@ export default {
 <template>
   <div class="card">
     <img :src="urlImg" alt="" />
-    <p>{{ carType }}</p>
-    <p>2 listings</p>
+    <p class="type">{{ carType }}</p>
+    <p class="listings">2 listings</p>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/partials/variables" as *;
 .card {
+  border: 1px solid $gray-main;
   width: calc(100% / 6);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  padding-bottom: 1.3rem;
+  .type {
+    font-size: 1.3rem;
+  }
+  .listings {
+    font-size: 0.7rem;
+    color: $gray-main;
+  }
 }
 </style>
