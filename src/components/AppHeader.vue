ce51a669fd6eaf798a01897ee522e77f024b4764
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-29 15:52:35 +0200

    add logo in navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,27 @@
 <script>
-export default {};
+export default {
+  props: {
+    urlImg: String,
+    navbarList: String,
+  },
+};
 </script>
 <template>
   <header>
     <div class="container">
       <ul class="navbar">
-        <li></li>
+        <li>
+          <a href="#"
+            ><img src="../assets/img/logo-autocar11.png" alt="Logo Autocar"
+          /></a>
+        </li>
       </ul>
     </div>
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  height: 70px;
+}
+</style>
