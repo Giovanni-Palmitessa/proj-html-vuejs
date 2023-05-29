b509a11965cad2a0b2350647b1f253da3cc701a9
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-29 16:59:04 +0200

    add style to jumbo details

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,9 +6,9 @@ export default {};
     <section class="jumbo">
       <div class="container">
         <div class="jumbo-details">
-          <p>Buy And Sell Your Car At Its Value</p>
-          <p>Find the right price and dealer.</p>
-          <p>Learn More -></p>
+          <p class="jumbo-main">Buy And Sell Your Car At Its Value</p>
+          <p class="jumbo-second">Find the right price and dealer.</p>
+          <p class="jumbo-second">Learn More -></p>
           <!--AGGIUNGERE ICONA FRECCIA -->
         </div>
       </div>
@@ -17,9 +17,20 @@ export default {};
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/partials/variables" as *;
 .jumbo {
   height: 100vh;
   background-image: url(../assets/img/slider-autocar-5.jpg);
   background-size: cover;
+  color: $white-main;
+  .jumbo-main {
+    font-size: 2rem;
+    font-weight: 800;
+    max-width: 360px;
+  }
+  .jumbo-second {
+    font-size: 0.9rem;
+    font-weight: 600;
+  }
 }
 </style>
