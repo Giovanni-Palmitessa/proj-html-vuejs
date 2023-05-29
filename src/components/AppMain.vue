d75f010b893b9dcbe7619d886d40bd5661d17728
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-29 16:49:01 +0200

    add background image to jumbo

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,18 @@
 <script>
 export default {};
 </script>
-<template></template>
+<template>
+  <main>
+    <section class="jumbo">
+      <div class="container"></div>
+    </section>
+  </main>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.jumbo {
+  height: 100vh;
+  background-image: url(../assets/img/slider-autocar-5.jpg);
+  background-size: cover;
+}
+</style>
