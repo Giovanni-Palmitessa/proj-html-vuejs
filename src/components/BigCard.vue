559608afbe3805dcd0ea8b3b40d8eefd1259967c
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-30 09:19:26 +0200

    add style to card

--- src/components/BigCard.vue
+++ src/components/BigCard.vue
@@ -7,12 +7,18 @@ export default {};
       src="../assets/img/bmw-8-series-convertible-technical-data-sp-desktop-400x300.jpg"
       alt=""
     />
-    <p>BMW M9 <font-awesome-icon icon="fa-solid fa-circle-check" /></p>
-    <p>Cabrio</p>
+    <p>
+      BMW M9 <font-awesome-icon icon="fa-solid fa-circle-check" class="icon" />
+    </p>
+    <p class="carType">Cabrio</p>
     <div class="shop">
-      <p><font-awesome-icon icon="fa-solid fa-dollar-sign" /> 34000</p>
-      <p><font-awesome-icon icon="fa-solid fa-car" /> BMW</p>
-      <p><font-awesome-icon icon="fa-solid fa-gas-pump" /> Electric</p>
+      <p>
+        <font-awesome-icon icon="fa-solid fa-dollar-sign" class="icon" /> 34000
+      </p>
+      <p><font-awesome-icon icon="fa-solid fa-car" class="icon" /> BMW</p>
+      <p>
+        <font-awesome-icon icon="fa-solid fa-gas-pump" class="icon" /> Electric
+      </p>
     </div>
   </div>
 </template>
@@ -24,6 +30,20 @@ export default {};
   border: 1px solid gray;
   img {
     width: 100%;
+    margin-bottom: 8px;
+  }
+  .carType {
+    font-size: 0.8rem;
+    margin-top: 8px;
+  }
+  .shop {
+    display: flex;
+    gap: 0.5rem;
+    font-size: 0.8rem;
+    margin-top: 8px;
+    .icon {
+      width: 10px;
+    }
   }
 }
 </style>
