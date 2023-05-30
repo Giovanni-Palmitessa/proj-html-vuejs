0dee2f19d09ea3506df8ce8e904edd4ceab7f9bc
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-30 16:34:52 +0200

    complete ftr left

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,12 +8,34 @@ export default {};
         <div class="ftr-left">
           <img src="../assets/img/logo-footer-autocar11.png" alt="Autocar" />
           <div class="contact">
-            <font-awesome-icon icon="fa-solid fa-location-dot" />
+            <font-awesome-icon icon="fa-solid fa-location-dot" class="icon" />
             <p>New Jersey,USA</p>
           </div>
+
           <div class="contact">
-            <font-awesome-icon icon="fa-solid fa-phone-volume" />
+            <font-awesome-icon icon="fa-solid fa-phone-volume" class="icon" />
             <p>+1 (234) 567 89 10</p>
+          </div>
+
+          <div class="contact">
+            <font-awesome-icon icon="fa-solid fa-envelope" class="icon" />
+            <p>example@example.com</p>
+          </div>
+
+          <div class="socials">
+            <a href="#"
+              ><font-awesome-icon icon="fa-brands fa-facebook" class="icon"
+            /></a>
+            <a href="#"
+              ><font-awesome-icon
+                icon="fa-brands fa-square-instagram"
+                class="icon"
+            /></a>
+            <a href="#"
+              ><font-awesome-icon
+                icon="fa-brands fa-square-twitter"
+                class="icon"
+            /></a>
           </div>
         </div>
       </div>
@@ -34,10 +56,25 @@ footer {
     .ftr-left {
       display: flex;
       flex-direction: column;
+      img {
+        width: 40%;
+        margin-bottom: 1rem;
+      }
       .contact {
         display: flex;
         gap: 10px;
         margin-bottom: 1rem;
+        .icon {
+          font-size: 1.5rem;
+        }
+      }
+      .socials {
+        display: flex;
+        gap: 15px;
+        font-size: 1.5rem;
+        a {
+          color: $white-main;
+        }
       }
     }
   }
