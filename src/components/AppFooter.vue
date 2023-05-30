ca41403017cf5900931d9984a19feac5228b52dd
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-30 16:03:27 +0200

    add logo in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,27 @@
 <script>
 export default {};
 </script>
-<template></template>
+<template>
+  <footer>
+    <div class="container">
+      <div class="footerContent">
+        <div class="ftr-left">
+          <img src="../assets/img/logo-footer-autocar11.png" alt="Autocar" />
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/partials/variables" as *;
+
+footer {
+  background-color: $black-main;
+  padding: 2rem;
+  .footerContent {
+    display: flex;
+    gap: 3rem;
+  }
+}
+</style>
